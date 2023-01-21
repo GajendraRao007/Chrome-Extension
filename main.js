fbdf403260933eee521d0dc04d11151175c32875
Gajendra <raogajendra27@gmail.com>
2023-01-21 14:02:49 +0530

    Used Arrays,For loop, Eventlistners!

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 let mylead = []
 const inputEl = document.getElementById("input-el")
 const Savebtn = document.getElementById("input-btn")
+const ulEl =document.getElementById("ul-el")
 
 Savebtn.addEventListener("click",function(){
-    console.log("Btn Clicked by eventlistner")
+    mylead.push(inputEl.value)
+    console.log(mylead)
 })
+
+for (let i = 0 ; i < mylead.length ; i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
