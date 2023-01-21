b411c2befcf7a6589b2d04dc04e106a14a338255
Gajendra <raogajendra27@gmail.com>
2023-01-21 12:35:59 +0530

    Let ,const, EventListner!

---
+++ main.js
@@ -0,0 +1,7 @@
+let mylead = []
+const inputEl = document.getElementById("input-el")
+const Savebtn = document.getElementById("input-btn")
+
+Savebtn.addEventListener("click",function(){
+    console.log("Btn Clicked by eventlistner")
+})
