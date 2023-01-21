06c8023407757dc9b26239ecf2fa4a1f03077411
Gajendra <raogajendra27@gmail.com>
2023-01-21 14:34:53 +0530

    Renderleads()

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 let mylead = []
 const inputEl = document.getElementById("input-el")
-const Savebtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
 const ulEl =document.getElementById("ul-el")
 
-Savebtn.addEventListener("click",function(){
+inputBtn.addEventListener("click",function(){
     mylead.push(inputEl.value)
-    console.log(mylead)
+    renderleads()
 })
 
+function renderleads(){
+
+let listitems = ""
 for (let i = 0 ; i < mylead.length ; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+   listitems += "<li>" + mylead[i] + "</li>"
 }
+ulEl.innerHTML = listitems
+
+}
+
